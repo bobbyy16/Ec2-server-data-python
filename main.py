e33563d3f99af51f6d6f52b66af005a324d93fa6
bobbyy16 <bobbyyyyy16@gmail.com>
2023-01-11 10:29:07 +0530

    added schedule library and scheduled a function to run every 5 mins

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # First spin up some servers then execute this code with region
 
 import boto3
+import schedule
 
 ec2_client = boto3.client('ec2')
 ec2_resource = boto3.resource('ec2')
@@ -23,9 +24,19 @@ for status in statuses['InstanceStatuses']:
 
 # print running state and status check in one call
 
-statuses = ec2_client.describe_instance_status()
-for status in statuses['InstanceStatuses']:
-    instance_status = status['InstanceStatus']['Status']
-    system_status = status['SystemStatus']['Status']
-    state = status['InstanceState']['Name']
-    print(f"Instance {status['InstanceId']} is {status} with instance status {instance_status} and system status {system_status}")
+def check_instance_status(): # This function is used to schedule a function
+    statuses = ec2_client.describe_instance_status(
+        IncludeAllInstances=True
+    )
+    for status in statuses['InstanceStatuses']:
+        instance_status = status['InstanceStatus']['Status']
+        system_status = status['SystemStatus']['Status']
+        state = status['InstanceState']['Name']
+        print(f"Instance {status['InstanceId']} is {status} with instance status {instance_status} and system status {system_status}")
+        print("#############\n")
+
+schedule.every(5).minutes.do(check_instance_status)
+# schedule.every().monday.at("12:00")
+
+while True:
+    schedule.run_pending()
