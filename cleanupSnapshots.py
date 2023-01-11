f337d97057fa3e04fca50da75b216cfb5acc4cfe
bobbyy16 <bobbyyyyy16@gmail.com>
2023-01-11 13:06:13 +0530

    added cleanUp - snapshots

--- cleanupSnapshots.py
+++ cleanupSnapshots.py
@@ -1,19 +1,38 @@
 from operator import itemgetter
-
+import schedule
 import boto3
 
 
 ec2_client = boto3.client('ec2', region_name = "eu-west-3")
 
-snapshots = ec2_client.describe_snapshots(
-    OwnerIds = ['self'] #only snapshots created by us
+
+volumes = ec2_client.describe_volumes(
+    # only create snapshots for prod / dev / testing servers
+    Filter = [
+        {
+            'Name': 'tag:Name',
+            'Values': ['prod']
+        }
+    ]
 )
 
-sortedBYDate = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)
-
-# snapshot deletion logic
-
-for snap in sortedBYDate[2:]: #select every snapshot excluding lastest 2
-    ec2_client.delete_snapshot(
-        SnapshotId=snap['SnapshotId']
+for volume in volumes['Volumes']:
+    snapshots = ec2_client.describe_snapshots(
+        OwnerIds = ['self'], #only snapshots created by us
+        Filter = [
+            {
+                'Name': 'volume-id',
+                'Values': [volume['VolumeId']]
+            }
+        ]
     )
+
+    sortedBYDate = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)
+
+    # snapshot deletion logic
+
+    for snap in sortedBYDate[2:]: #select every snapshot excluding lastest 2
+        res= ec2_client.delete_snapshot(
+            SnapshotId=snap['SnapshotId']
+        )
+        print(res)
